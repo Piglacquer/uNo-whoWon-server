5bca44a88cbed32c0949acfe59d7d82f2511d2da
mrsaxmannjr <mrsaxmannjr@gmail.com>
2017-12-14 13:46:49 -0700

    added port.env

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.post("/", (request, response) => {
   response.json("post worked");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port 3000');
 });
