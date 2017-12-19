4faf6f2fac3700bdf38932c8ad9dbff957322082
Patrick Biffle <pbiffle@gmail.com>
2017-12-19 15:43:03 -0700

    attempting to deploy server

--- Data/data.js
+++ Data/data.js
@@ -1,8 +1,61 @@
-var dataSet = [{
-  id: 1,
-  name: "James",
-}];
+var games = [{
+  gameId: 1,
+  gameWinnerId: 1,
+  numberofPlayers: 5
+}, {
+  gameId: 2,
+  gameWinnerId: 2,
+  numberofPlayers: 5
+}, {
+  gameId: 3,
+  gameWinnerId: 6,
+  numberofPlayers: 5
+}]
+
+var players = [{
+  playerId: 1,
+  playerName: 'Collin',
+  wins: 0,
+  scoreThisGame: 0
+}, {
+  playerId: 2,
+  playerName: 'Taylor',
+  wins: 0,
+  scoreThisGame: 0
+}, {
+  playerId: 3,
+  playerName: 'Patrick',
+  wins: 0,
+  scoreThisGame: 0
+}, {
+  playerId: 4,
+  playerName: 'Julie',
+  wins: 0,
+  scoreThisGame: 0
+}, {
+  playerId: 5,
+  playerName: 'Debbie',
+  wins: 0,
+  scoreThisGame: 0
+}, {
+  playerId: 6,
+  playerName: 'David',
+  wins: 0,
+  scoreThisGame: 0
+}, {
+  playerId: 7,
+  playerName: 'Luke',
+  wins: 0,
+  scoreThisGame: 0
+}, {
+  playerId: 8,
+  playerName: 'Joey',
+  wins: 0,
+  scoreThisGame: 0
+}]
+
 
 module.exports = {
-  dataSet
-};
+  games,
+  players
+}
--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const bodyParser = require("body-parser");
-const data = require("./Data/data");
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const bodyParser = require("body-parser")
+const data = require("./Data/data")
+const games = data.games
+const players = data.players
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get("/", (request, response) => {
-  response.json(data.dataSet[0].name);
-});
+  response.json(players)
+})
 
 app.post("/", (request, response) => {
-  response.json(request.body);
-});
+  response.json('post worked')
+})
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on port 3000');
-});
+  console.log('listening on port 3000')
+})
